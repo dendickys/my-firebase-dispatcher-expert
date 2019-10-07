ec1073a2dfc72ed4b58a79326ad33d6602e78a2e
dendickys <febri.unquad2014@gmail.com>
2019-10-07 21:32:43 +0700

    Add MyJobService.class

--- app/src/main/java/com/dendickys/myfirebasedispatcherexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/myfirebasedispatcherexpert/MainActivity.java
@@ -3,12 +3,33 @@ package com.dendickys.myfirebasedispatcherexpert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSetScheduler, btnCancelScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSetScheduler = findViewById(R.id.btn_set_scheduler);
+        btnCancelScheduler = findViewById(R.id.btn_cancel_scheduler);
+
+        btnSetScheduler.setOnClickListener(this);
+        btnCancelScheduler.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_set_scheduler) {
+            Toast.makeText(this, "Dispatcher Created", Toast.LENGTH_SHORT).show();
+        }
+        if (v.getId() == R.id.btn_cancel_scheduler) {
+            Toast.makeText(this, "Dispatcher Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
